0c04822541bddbb589c68d2193a39eeda7dc69ec
mohammed <mohammeddolapo06@gmail.com>
2023-06-15 16:41:25 +0100

    version 1

---
+++ components/Footer.tsx
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import React from "react";
+import { footerLinks } from "@/constants";
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
+      <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
+        <div className="flex flex-col justify-start items-start gap-6">
+          <Image
+            src="/lao.png"
+            alt="logo"
+            width={118}
+            height={18}
+            className="object-contain"
+          />
+          <p className="text-base text-gray-700">
+            Lao-bells Car hub <br />
+            All rights reserved &copy;
+          </p>
+        </div>
+      </div>
+
+      <div className="footer__links">
+        {footerLinks.map((link) => (
+          <div className="footer__link" key={link.title}>
+            <h3 className="font-bold"> {link.title}</h3>
+            {link.links.map((item) => (
+              <Link href={item.url} key={item.title} className="text-gray-500">
+                {item.title}
+              </Link>
+            ))}
+          </div>
+        ))}
+      </div>
+      <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
+        <p>@2023 Lao-Bells Carhub . All Rights Reserved</p>
+        <div className="footer__copyrights-link">
+          <Link href="/" className="text-gray-500">
+            Privacy policy
+          </Link>
+          <Link href="/" className="text-gray-500">
+            Terms of Use
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ utils/index.ts
@@ -0,0 +1,34 @@
+import { CarProps } from "@/types";
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+  const basePricePerDay = 50; // Base rental price per day in dollars
+  const mileageFactor = 0.1; // Additional rate per mile driven
+  const ageFactor = 0.05; // Additional rate per year of vehicle age
+
+  // Calculate additional rate based on mileage and age
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  // Calculate total rental rate per day
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+  return rentalRatePerDay.toFixed(0);
+};
+
+export async function fetchCars() {
+  const headers = {
+    "X-RapidAPI-Key": "a533a8f926mshfacf80aaad1e9e9p17f4c5jsn3fedd5a419ad",
+    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+  };
+
+  const response = await fetch(
+    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla",
+    {
+      headers: headers,
+    }
+  );
+
+  const result = await response.json();
+
+  return result;
+}
